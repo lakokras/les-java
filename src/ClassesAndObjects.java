2d2ca2ae556527cdc3e41256c87c0ee22b344e62
Arthur <chupryna.2004@mail.ru>
2023-05-12 02:00:42 +0300

    setGet

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,31 +1,54 @@
-public class ClassesAndObjects {
-    public static void main (String[] args) {
+public class ClassesAndObjects{
+    public static void main(String[] args){
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
-        Person person2 = new Person();
-        person2.name = "Вова";
-        person2.age = 20;
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии: " + year1 + " лет.");
-        System.out.println("Второму человеку до пенсии: " + year2 + " лет.");
+        person1.setName("Какое-то имя");
+        person1.setAge(-12);
+        System.out.println
+                ("Выводим значения в main методе: " + person1.getName());
+        System.out.println
+                ("Выводим значения в main методе: " + person1.getAge());
     }
 }
 
-class Person {
-    String name;
-    int age;
+// У класса могут быть:
+// 1. Данные (поля)
+// 2. Действия, которые он может совершать (методы)
 
-    int calculateYearsToRetirement() {
-        int years = 65 - age;
-        return years;
+class Person{
+    private String name;
+    private int age;
+
+    public void setName(String userName){
+        if (userName.isEmpty()){
+            System.out.println("Ты ввел пустое имя.");
+        } else {
+            name = userName;
+        }
+    }
+    public String getName(){
+        return name;
     }
 
-    void speak() {
-        for(int i = 0; i < 3; i++) {
-            System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
+    public void setAge(int userAge){
+        if (userAge < 0){
+            System.out.println
+                    ("Возраст должен быть положительным.");
+        } else {
+            age = userAge;
         }
+    }
+    public int getAge(){
+        return age;
+    }
+
+    int calculateYearsToRetirement() {
+        return 65 - age;
+    }
+
+    void speak(){
+        for(int i = 0; i < 3; i++)
+            System.out.println
+                    ("Меня зовут " + name + ", мне " + age + " лет.");
     }
     void sayHello() {
         System.out.println("Привет!");
